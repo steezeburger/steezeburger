f65e1b0bf5dd3ab6725712524ff1c8a63ae6df90
steezeburger <jessetsnyder@gmail.com>
2014-10-18 20:49:45 -0500

    set mimetype equal to jpeg to allow users to right click to save as a .jpg

--- app/glitchy.py
+++ app/glitchy.py
@@ -33,8 +33,8 @@ def glitch(image):
 
     for i in range (1, random.randint(1,10)):
     # for i in range (1, 10):
-       #data = splice_file(data)
-        print 'it is working'
+       data = splice_file(data)
+        # print 'it is working'
 
     image_file = open(image, 'w')
     image_file.write(data)
